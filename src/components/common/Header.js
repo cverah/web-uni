8ac5465d651e405533b2491c3ce4180f1e55f710
Cristhian Vera <cristianmario092190@gmail.com>
2024-03-03 12:19:24 -0500

    fix: text in json

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,51 +1,65 @@
-import React, { useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import Hamburguesa from '../../pages/Noticias/Imagenes/Icons/MenuHambuerguesa.svg';
-import '../../styles/Header.css';
+import React, { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import Hamburguesa from "../../pages/Noticias/Imagenes/Icons/MenuHambuerguesa.svg";
+import "../../styles/Header.css";
 
 function Header() {
+  const [valorMenu, setMenu] = useState(false);
+  const mostrarMenu = () => {
+    setMenu(!valorMenu);
+  };
 
-    const [ valorMenu, setMenu] = useState(false)
-    const mostrarMenu = () => {
-        setMenu( !valorMenu )
-    } 
+  return (
+    <header className="encabezado">
+      <figure className="logos-header">
+        <img
+          className="logo"
+          src={require("../../pages/Noticias/Imagenes/Logo1.jpg")}
+          alt="Logo1"
+        />
+        <img
+          className="logo"
+          src={require("../../pages/Noticias/Imagenes/Logo2.jpg")}
+          alt="Logo2"
+        />
+      </figure>
 
-    return (
-        <header className='encabezado'>
-            
-            <figure className='logos-header'>
-                <img className="logo" src={require('../../pages/Noticias/Imagenes/Logo1.jpg')} alt="Logo1" />
-                <img className="logo" src={require('../../pages/Noticias/Imagenes/Logo2.jpg')} alt="Logo2" />
-            </figure>
-                    
-            <button
-              onClick={ mostrarMenu }  
-              className='encabezado-button'>
-              <img className='encabezado-svg' src={Hamburguesa} />
-            </button>
+      <button onClick={mostrarMenu} className="encabezado-button">
+        <img className="encabezado-svg" src={Hamburguesa} alt="img" />
+      </button>
 
-            <nav className={ `encabezado-nav ${ valorMenu ? 'isActive' : '' }` }>
-                <ul className='encabezado-ul'>
-                    <li className='encabezado-li'>
-                        <Link className='encabezado-link' to='/'>INICIO</Link>
-                    </li>
-                    <li className='encabezado-li'>
-                        <Link className='encabezado-link' to='/PaginaNoticias'>NOTICIAS</Link>
-                    </li>
-                    <li className='encabezado-li'>
-                        <Link className='encabezado-link' to='/PaginaComunidad'>COMUNIDAD</Link>
-                    </li>
-                    <li className='encabezado-li'>
-                        <Link className='encabezado-link' to='/PaginaProyectos'>PROYECTOS</Link>
-                    </li>
-                    <li className='encabezado-li'>
-                        <Link className='encabezado-link' to='/PaginaContactos'>CONTACTOS</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Outlet /> 
-        </header>
-    )
+      <nav className={`encabezado-nav ${valorMenu ? "isActive" : ""}`}>
+        <ul className="encabezado-ul">
+          <li className="encabezado-li">
+            <Link className="encabezado-link" to="/">
+              INICIO
+            </Link>
+          </li>
+          <li className="encabezado-li">
+            <Link className="encabezado-link" to="/PaginaNoticias">
+              NOTICIAS
+            </Link>
+          </li>
+          <li className="encabezado-li">
+            <Link className="encabezado-link" to="/PaginaComunidad">
+              COMUNIDAD
+            </Link>
+          </li>
+          <li className="encabezado-li">
+            <Link className="encabezado-link" to="/PaginaProyectos">
+              PROYECTOS
+            </Link>
+          </li>
+          <li className="encabezado-li">
+            <Link className="encabezado-link" to="/PaginaContactos">
+              CONTACTOS
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </header>
+  );
 }
 
 export default Header;
--- src/pages/Noticias/NavegacionNoticias.js
+++ src/pages/Noticias/NavegacionNoticias.js
@@ -1,47 +1,72 @@
-import React from 'react';
-import '../../styles/Noticias/NavegacionNoticias.css';
+import React from "react";
+import "../../styles/Noticias/NavegacionNoticias.css";
+import { Link } from "react-router-dom";
 
-function NavegacionNoticias ({noticiasPorPagina, currentNoticias, setCurrentNoticias, totalNoticias}) {
+function NavegacionNoticias({
+  noticiasPorPagina,
+  currentNoticias,
+  setCurrentNoticias,
+  totalNoticias,
+}) {
+  const numerosPagina = [];
 
-    const numerosPagina = []
+  for (let i = 1; i <= Math.ceil(totalNoticias / noticiasPorPagina); i++) {
+    numerosPagina.push(i);
+  }
 
-    for (let i = 1; i  <= Math.ceil(totalNoticias / noticiasPorPagina); i++){
-        numerosPagina.push(i)
-    }
+  const anteriorPagina = () => {
+    setCurrentNoticias(currentNoticias - 1);
+  };
 
-    const anteriorPagina = () => {
-        setCurrentNoticias(currentNoticias - 1)
-    }
+  const siguientePagina = () => {
+    setCurrentNoticias(currentNoticias + 1);
+  };
 
-    const siguientePagina = () => {
-        setCurrentNoticias(currentNoticias + 1)
-    }
+  const paginaEspecifica = (n) => {
+    setCurrentNoticias(n);
+  };
 
-    const paginaEspecifica =(n) => {
-        setCurrentNoticias(n)
-    }
+  return (
+    <nav aria-label="..." className="navegacion-noticias">
+      <ul className="pagination">
+        <li className="page-item">
+          <Link
+            className={`page-link ${currentNoticias === 1 ? "disabled" : ""}`}
+            to="#"
+            onClick={anteriorPagina}
+          >
+            Anterior
+          </Link>
+        </li>
 
-    return (
-        <nav  aria-label='...' className='navegacion-noticias'>
-            <ul className='pagination'>
+        {numerosPagina.map((paginaNo) => (
+          <li key={paginaNo}>
+            <Link
+              className={`page-link ${
+                paginaNo === currentNoticias ? "active" : ""
+              }`}
+              href="#"
+              onClick={() => paginaEspecifica(paginaNo)}
+            >
+              {paginaNo}
+            </Link>
+          </li>
+        ))}
 
-                <li className='page-item'>
-                    <a className={`page-link ${currentNoticias === 1 ? 'disabled' : ''}`} href='#' onClick={anteriorPagina}>Anterior</a>
-                </li>
-
-                {numerosPagina.map(paginaNo => (
-                    <li key={paginaNo} >
-                        <a className={`page-link ${paginaNo === currentNoticias ? 'active' : ''}`} href='#' onClick={() => paginaEspecifica(paginaNo)}>{paginaNo}</a>
-                    </li>
-                ))}
-
-                <li className='page-item'>
-                    <a className={`page-link ${currentNoticias >= numerosPagina.length ? 'disabled' : ''}`} href='#' onClick={siguientePagina}>Siguiente</a>
-                </li>
-
-            </ul>
-        </nav>
-    );
+        <li className="page-item">
+          <a
+            className={`page-link ${
+              currentNoticias >= numerosPagina.length ? "disabled" : ""
+            }`}
+            href="#"
+            onClick={siguientePagina}
+          >
+            Siguiente
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavegacionNoticias;
