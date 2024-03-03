217776a74d556b423b12e1870fb729b56c5f26dd
Cristhian Vera <cristianmario092190@gmail.com>
2024-03-03 12:31:30 -0500

    fix: set in usetate

--- src/components/common/TarjetaNoticia.js
+++ src/components/common/TarjetaNoticia.js
@@ -1,62 +1,69 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import NavegacionNoticias from "../../pages/Noticias/NavegacionNoticias";
 import Noticieros from "../../pages/Noticias/Noticieros";
-import '../../styles/Noticias/TarjetaNoticia.css'
+import "../../styles/Noticias/TarjetaNoticia.css";
 
-function TarjetaNoticia () {
+function TarjetaNoticia() {
+  const [noticias, setNoticias] = useState(Noticieros);
+  const [noticiasPorPagina, setNoticiasPorPagina] = useState(8);
+  const [currentNoticias, setCurrentNoticias] = useState(1);
 
-  const [noticias, setNoticias] = useState(Noticieros)
-  const [noticiasPorPagina, setNoticiasPorPagina] = useState(8) 
-  const [currentNoticias, setCurrentNoticias] = useState(1)
-
-  const totalNoticias = Noticieros.length 
-  const ultimoIndex = currentNoticias * noticiasPorPagina // 1 * 6 = 6
-  const primerIndex = ultimoIndex - noticiasPorPagina // 6 - 6 = 0
+  const totalNoticias = Noticieros.length;
+  const ultimoIndex = currentNoticias * noticiasPorPagina; // 1 * 6 = 6
+  const primerIndex = ultimoIndex - noticiasPorPagina; // 6 - 6 = 0
 
   const navigate = useNavigate();
 
+  console.log(setNoticias);
+  console.log(setNoticiasPorPagina);
   return (
     <div>
-      <main className='contenedor-secundario-noticias'>
+      <main className="contenedor-secundario-noticias">
         {/*MOSTRANDO TODAS LAS NOTICIAS - 6 POR PAGINA*/}
-        {noticias.map(notici => (
-        <section key={notici.id} className='contenedor-noticias'>
-
-            <figure>
+        {noticias
+          .map((notici) => (
+            <section key={notici.id} className="contenedor-noticias">
+              <figure>
                 {/*MOSTRANDO TITULO DE LA NOTICIA*/}
-                <div className='content-titulo'> 
-                    <p>{notici.titulo}</p>
+                <div className="content-titulo">
+                  <p>{notici.titulo}</p>
                 </div>
                 {notici.imagen.map((notic, index) => (
-                <div className='content-img-noticias' key={index}>
-                  {/*MOSTRANDO IMAGEN DE LA TARJETA*/}
-                  <img src={notic.imgs} alt='Foto de noticia' />  
-                </div>
+                  <div className="content-img-noticias" key={index}>
+                    {/*MOSTRANDO IMAGEN DE LA TARJETA*/}
+                    <img src={notic.imgs} alt="Foto de noticia" />
+                  </div>
                 ))}
-            </figure>
-            {/*MOSTRANDO NOTICIA*/}
-            <div className='texto-noticia'>
+              </figure>
+              {/*MOSTRANDO NOTICIA*/}
+              <div className="texto-noticia">
                 <p>{notici.texto}</p>
-            </div>
-            {/*PASANDO PARAMETROS PARA LA PAGINA NOTICIAS INDIVIDUALES*/}
-            <button type="button" class="btn btn-dark" onClick={() => navigate(`/NoticiasIndividuales/${notici.id}`)}>Leer más</button>
-        </section>
-        )).slice(primerIndex, ultimoIndex)} 
+              </div>
+              {/*PASANDO PARAMETROS PARA LA PAGINA NOTICIAS INDIVIDUALES*/}
+              <button
+                type="button"
+                class="btn btn-dark"
+                onClick={() => navigate(`/NoticiasIndividuales/${notici.id}`)}
+              >
+                Leer más
+              </button>
+            </section>
+          ))
+          .slice(primerIndex, ultimoIndex)}
       </main>
 
-  <div >
-      {/*LLAMANDO AL COMPONENTE DE PAGINACION NAVEGACIONNOTICIAS Y ENVIADO PARAMETROS*/}
-      <NavegacionNoticias 
-          noticiasPorPagina={noticiasPorPagina} 
-          currentNoticias={currentNoticias} 
-          setCurrentNoticias={setCurrentNoticias} 
+      <div>
+        {/*LLAMANDO AL COMPONENTE DE PAGINACION NAVEGACIONNOTICIAS Y ENVIADO PARAMETROS*/}
+        <NavegacionNoticias
+          noticiasPorPagina={noticiasPorPagina}
+          currentNoticias={currentNoticias}
+          setCurrentNoticias={setCurrentNoticias}
           totalNoticias={totalNoticias}
-      />
-  </div>
-</div>
-
-)
+        />
+      </div>
+    </div>
+  );
 }
 
 export default TarjetaNoticia;
--- src/components/common/TarjetaProyecto.js
+++ src/components/common/TarjetaProyecto.js
@@ -1,45 +1,42 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Proyectos from "../../pages/Proyectos/Proyectos";
-import '../../styles/Proyectos/TarjetaProyecto.css'
+import "../../styles/Proyectos/TarjetaProyecto.css";
 
-function TarjetaProyecto () {
+function TarjetaProyecto() {
+  const [proyectos, setProyectos] = useState(Proyectos);
 
-  const [proyectos, setProyectos] = useState(Proyectos)
+  console.log(setProyectos);
 
   return (
     <div>
-      <main className='contenedor-general-proyectos'>
+      <main className="contenedor-general-proyectos">
         {/*MOSTRANDO TARJETAS DE PROYECTOS*/}
-        {proyectos.map(proyect => (
-        <section key={proyect.id} className='contenedor-proyectos'>
-
-            <figure >
-                <div className='contenedor-imagen'>
-                  {/*MOSTRANDO IMAGEN DEL PROYECTO*/}
-                    <img 
-                      src={proyect.imagen} 
-                      alt='Foto de noticia'
-                    />
-                </div>
+        {proyectos.map((proyect) => (
+          <section key={proyect.id} className="contenedor-proyectos">
+            <figure>
+              <div className="contenedor-imagen">
+                {/*MOSTRANDO IMAGEN DEL PROYECTO*/}
+                <img src={proyect.imagen} alt="Foto de noticia" />
+              </div>
             </figure>
 
             <div className="contenedor-textos">
-                {/*MOSTRANDO TITULO DEL PROYECTO*/}
-                {/*PASANDO PARAMETRO PARA LA PAGINA PROYECTOS INDIVIDUALES*/}
-                <Link to={`/ProyectosIndividuales/${proyect.id}`}>{proyect.titulo}</Link>
-                {/*MOSTRANDO TEXTO DEL PROYECTO*/}
-                <div className='texto-proyecto'>
-                  <p>{proyect.proyectext}</p>
-                </div>
+              {/*MOSTRANDO TITULO DEL PROYECTO*/}
+              {/*PASANDO PARAMETRO PARA LA PAGINA PROYECTOS INDIVIDUALES*/}
+              <Link to={`/ProyectosIndividuales/${proyect.id}`}>
+                {proyect.titulo}
+              </Link>
+              {/*MOSTRANDO TEXTO DEL PROYECTO*/}
+              <div className="texto-proyecto">
+                <p>{proyect.proyectext}</p>
+              </div>
             </div>
-            
-        </section>
-        ))} 
-  </main>
-</div>
-
-)
+          </section>
+        ))}
+      </main>
+    </div>
+  );
 }
 
 export default TarjetaProyecto;
