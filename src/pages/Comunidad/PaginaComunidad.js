ed4255b955e59deeb75ac4cc57b0e6e34fff276a
Cristhian Vera <cristianmario092190@gmail.com>
2024-04-02 23:08:06 -0500

    feat: add section nosotros

--- src/pages/Comunidad/PaginaComunidad.js
+++ src/pages/Comunidad/PaginaComunidad.js
@@ -4,13 +4,15 @@ import WelcomeComunity from "./WelcomeComunity";
 import "../../styles/Comunidad/comunidad.css";
 import Mision from "./Mision";
 import Vision from "./Vision";
+import ImgCohete from "./images/cohete.png";
 
 const PaginaComunidad = () => {
   return (
     <>
       <Header />
+
+      <WelcomeComunity />
       <div className="container_comunidad">
-        <WelcomeComunity />
         <Mision />
         <Vision />
       </div>
--- src/pages/Comunidad/WelcomeComunity.js
+++ src/pages/Comunidad/WelcomeComunity.js
@@ -7,77 +7,100 @@ import IconTwitter from "./icons/BiTwitterX.svg";
 import IconPerson from "./icons/SolarPeopleNearbyBroken.svg";
 import IconTiktok from "./icons/IcBaselineTiktok.svg";
 import IconDiscord from "./icons/BiDiscord.svg";
+import { Link } from "react-router-dom";
 
 const WelcomeComunity = () => {
   return (
     <div className="container_welcome">
       <div className="welcome">
-        <h4>Bienvenido a nuestra comunidad</h4>
         <div className="nosotros">
-          <h1>Nosotros</h1>
-          <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h1>
+          <h1>Nuestra Organizacion</h1>
+
           <p className="parrafo">
-            conoce nuestra historia Lorem ipsum dolor sit amet!
+            En IEE AES, nuestra pasión es encender la chispa de la curiosidad y
+            la innovación en las mentes jóvenes. Estamos comprometidos a
+            inspirar a la siguiente generación de exploradores espaciales
+            mediante programas educativos interactivos y atractivos. Nuestro
+            programa CanSat Explorers está diseñado especialmente para motivar y
+            desafiar a estudiantes de tercero, cuarto y quinto de secundaria,
+            guiándolos a través del emocionante proceso de diseñar, construir y
+            lanzar su propio picosatélite.
           </p>
         </div>
       </div>
+      <h1>Nuestras Redes Sociales</h1>
       <div className="welcome_mediasocial">
-        <div className="card_mediasocial">
-          <img src={IconFacebook} alt="iconfacebook" />
-          <div className="information">
-            <p>48 k</p>
-            <p>Followers</p>
+        <Link className="link_mediasocial" to="#">
+          <div className="card_mediasocial">
+            <img src={IconFacebook} alt="iconfacebook" />
+            <div className="information">
+              <p>48 k</p>
+              <p>Followers</p>
+            </div>
           </div>
-        </div>
+        </Link>
 
-        <div className="card_mediasocial">
-          <img src={IconInstagram} alt="iconfacebook" />
-          <div className="information">
-            <p>48 k</p>
-            <p>Followers</p>
+        <Link className="link_mediasocial" to="#">
+          <div className="card_mediasocial">
+            <img src={IconInstagram} alt="iconfacebook" />
+            <div className="information">
+              <p>48 k</p>
+              <p>Followers</p>
+            </div>
           </div>
-        </div>
+        </Link>
 
-        <div className="card_mediasocial">
-          <img src={Iconlinkedin} alt="iconfacebook" />
-          <div className="information">
-            <p>48 k</p>
-            <p>Followers</p>
+        <Link className="link_mediasocial" to="#">
+          <div className="card_mediasocial">
+            <img src={Iconlinkedin} alt="iconfacebook" />
+            <div className="information">
+              <p>48 k</p>
+              <p>Followers</p>
+            </div>
           </div>
-        </div>
+        </Link>
 
-        <div className="card_mediasocial">
-          <img src={IconTwitter} alt="iconfacebook" />
-          <div className="information">
-            <p>48 k</p>
-            <p>Followers</p>
+        <Link className="link_mediasocial" to="#">
+          <div className="card_mediasocial">
+            <img src={IconTwitter} alt="iconfacebook" />
+            <div className="information">
+              <p>48 k</p>
+              <p>Followers</p>
+            </div>
           </div>
-        </div>
+        </Link>
 
-        <div className="card_mediasocial">
-          <img src={IconPerson} alt="iconfacebook" />
-          <div className="information">
-            <p>48 k</p>
-            <p>Followers</p>
+        <Link className="link_mediasocial" to="#">
+          <div className="card_mediasocial">
+            <img src={IconPerson} alt="iconfacebook" />
+            <div className="information">
+              <p>48 k</p>
+              <p>Followers</p>
+            </div>
           </div>
-        </div>
+        </Link>
 
-        <div className="card_mediasocial">
-          <img src={IconTiktok} alt="iconfacebook" />
-          <div className="information">
-            <p>48 k</p>
-            <p>Followers</p>
+        <Link className="link_mediasocial" to="#">
+          <div className="card_mediasocial">
+            <img src={IconTiktok} alt="iconfacebook" />
+            <div className="information">
+              <p>48 k</p>
+              <p>Followers</p>
+            </div>
           </div>
-        </div>
+        </Link>
 
-        <div className="card_mediasocial">
-          <img src={IconDiscord} alt="iconfacebook" />
-          <div className="information">
-            <p>48 k</p>
-            <p>Followers</p>
+        <Link className="link_mediasocial" to="#">
+          <div className="card_mediasocial">
+            <img src={IconDiscord} alt="iconfacebook" />
+            <div className="information">
+              <p>48 k</p>
+              <p>Followers</p>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
+      <h1>Conoce mas de IEE AES</h1>
       <div className="welcome_links">
         <div className="links_menu">
           <a href="#mision">Mision</a>
